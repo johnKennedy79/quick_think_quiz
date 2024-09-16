37116727c4d13ea3486d52ceec4982047785ef90
NOAH SMART <noahsmart48@gmail.com>
2024-09-16 15:26:13 +0100

    sorted things out

--- src/app/components/tabs.jsx
+++ src/app/components/tabs.jsx
@@ -3,13 +3,22 @@ import * as Tabs from "@radix-ui/react-tabs";
 import "./tabs.css";
 
 const TabsDemo = () => (
-
   <Tabs.Root className="TabsRoot" defaultValue="tab1">
     <Tabs.List className="TabsList" aria-label="Manage your account">
-      <Tabs.Trigger className="TabsTrigger" value="tab1">
+      <Tabs.Trigger
+        className="TabsTrigger"
+        value="tab1"
+        title="About Quick Think"
+        tabIndex={0}
+      >
         About Quick Think
       </Tabs.Trigger>
-      <Tabs.Trigger className="TabsTrigger" value="tab2">
+      <Tabs.Trigger
+        className="TabsTrigger"
+        value="tab2"
+        title="Meet The Team"
+        tabIndex={0}
+      >
         Meet The Team
       </Tabs.Trigger>
     </Tabs.List>
@@ -23,19 +32,19 @@ const TabsDemo = () => (
       <h1> What is Quick Think? </h1>
       <p className="Text">
         Quick Think is an interactive web app designed to entertain and educate
-        through multiple-choice quizzes. We cover a varity of categories to keep
+        through multiple-choice quizzes. We cover a variety of categories to keep
         things fresh and exciting: Entertainment: Film - Explore trivia about
         iconic movies, actors, directors, and the magic behind the screen.
         Entertainment: Video Games - Test your gaming knowledge, from retro
         classics to modern blockbusters. General Knowledge - Show off your
-        smarts with questions spaning history, culturem geography, and more.
+        smarts with questions spanning history, culture, geography, and more.
         Science & Nature - Dive into the wonders of the natural world and the
         mysteries of the universe.
       </p>
       <p>
         {" "}
-        Each Quiz is timed, encourgaing quick thinking under pressure. Can you
-        beat the clock and claim the top spot on our leaderboard?
+        Each Quiz is timed, encouraging quick thinking under pressure. Can you
+        beat the clock and claim the top spot on our leader board?
       </p>
       <h1> Why Quick Think? </h1>
       <p>
--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Link from "next/link";
 import Footer from "./components/footer";
-import "./media.css"
+import "./media.css";
 
 const manname = localFont({
   src: "./fonts/Maname-Regular.ttf",
@@ -38,8 +38,12 @@ export default function RootLayout({ children }) {
             <h1>Quick Think</h1>
           </header>
           <nav>
-            <Link href="/">Home</Link>
-            <Link href="/About">About</Link>
+            <Link title="Home" tabIndex={0} href="/">
+              Home
+            </Link>
+            <Link title="About" tabIndex={0} href="/About">
+              About
+            </Link>
           </nav>
           <br />
           <SignedOut>
@@ -48,15 +52,19 @@ export default function RootLayout({ children }) {
           <SignedIn>
             <nav>
               {/* <b> */}
-              <Link href="/UserProfile">User Profile</Link>
-              <Link href="/Play">Play</Link>
+              <Link title="Profile" tabIndex={0} href="/UserProfile">
+                User Profile
+              </Link>
+              <Link title="Play" tabIndex={0} href="/Play">
+                Play
+              </Link>
               {/* </b> */}
             </nav>
           </SignedIn>
           {children}
-          <footer className="footer">
+          <footer title="Footer" className="footer">
             <Footer />
-     
+
             <UserButton className="signIn" />
             <SignedOut></SignedOut>
           </footer>
