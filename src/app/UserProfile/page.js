604c069b35bc05ea6d5bf099a744babaa15ebdb4
John MR Kennedy <john.m.r.kennedy.jk@gmail.com>
2024-09-19 19:36:28 +0100

    Update page.js
    final commit merge conflict resolved

--- src/app/UserProfile/page.js
+++ src/app/UserProfile/page.js
@@ -2,12 +2,11 @@ import { db } from "@/lib/db";
 import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 import { QuizProvider } from "@/context/QuizContext";
-// import ResultsChart from "../components/Results_Chart";
 import { RadarGraph } from "../components/Radar";
+import DataSet4 from "@/lib/resultData";
 
 export default async function UserProfile() {
   const user = await currentUser();
-  console.log(user);
 
   const result = await db.query(
     `SELECT * FROM quiz_users WHERE clerk_id = $1`,
@@ -19,7 +18,6 @@ export default async function UserProfile() {
       `INSERT INTO quiz_users (clerk_id, user_name, avatar) VALUES ($1, $2, $3)`,
       [user.id, user.username, user.imageUrl]
     );
-    // console.log("user.id, user.username, avatar");
 
     return (
       <div>
@@ -30,12 +28,9 @@ export default async function UserProfile() {
   }
 
   const existingUser = result.rows[0];
-  const data = await db.query(
-    `SELECT * FROM quiz_score_table WHERE clerk_id = $1`,
-    [existingUser.clerk_id]
-  );
-  const chartData = data.rows;
-  // console.log(chartData);
+
+  const dataSet = await DataSet4();
+ 
   return (
     <div className="w-2/3">
       <Image
@@ -49,8 +44,7 @@ export default async function UserProfile() {
       <p className="user">{existingUser.user_name}</p>
       <div>
         <QuizProvider>
-          {/* <ResultsChart chartData={chartData} /> */}
-          <RadarGraph />
+          <RadarGraph dataSet={dataSet} />
         </QuizProvider>
       </div>
     </div>
