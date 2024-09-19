8d783833536f21632c943b91095b34007dd9ebad
ZaynabLunat <lunat815@gmail.com>
2024-09-19 16:16:41 +0100

    c1

--- src/app/components/Questions.js
+++ src/app/components/Questions.js
@@ -19,6 +19,7 @@ export default function Questions({ quizQuestions, addResults, userId }) {
 
   useEffect(function () {
     setQuestions(quizQuestions);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
